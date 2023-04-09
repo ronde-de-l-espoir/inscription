fd8485a7b8113a122b68a271df8126fb6cf53518
Elias Kirkwood <ek.mindstorms@outlook.com>
2023-04-09 07:19:27 +0200

    Feat : nav

---
+++ modules/nav/nav.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="<?php echo $prefix . "modules/nav/nav.css" ?>">
+<header>
+    <div class="header-wrapper">
+        
+        <span class="title"><a href="https://ronde-de-l-espoir.fr/">La Ronde de l'Espoir</a></span>
+        <span class="arrow">
+            <a href="https://ronde-de-l-espoir.fr">
+                <span class="material-symbols-outlined">arrow_back</span>
+            </a>
+        </span>
+        
+        <span class="nav-image">
+            <img src="<?php echo $prefix ?>img/LRDE-logo.png" alt="">
+        </span>
+    </div>
+</header>
