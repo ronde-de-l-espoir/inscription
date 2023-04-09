39024a8e0a980f0a66485f2ab23d4e3a5993f376
Elias Kirkwood <ek.mindstorms@outlook.com>
2023-04-09 08:38:38 +0200

    Feat : work on activity selection page

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LRDE -- Inscription</title>
+    <link rel="stylesheet" href="./common.css">
+    <link rel="stylesheet" href="./home.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 <body>
@@ -14,6 +16,14 @@
     ?>
 
     <main>
+        <div id="welcome">
+            <h2>Bonjour !</h2>
+            <p>Cette plateforme vous permettra d'acheter vos tickets pour :</p>
+        </div>
+        <form action="./" method="post">
+            <button name="activity" id="gala-btn" value="gala">Gala</button>
+            <button name="activity" id="tombola-btn" value="tombola">Tombola</button>
+        </form>
     </main>
 </body>
 </html>
