ad474eb89a554555911a58c97e7c84d83e655ad5
Elias Kirkwood <ek.mindstorms@outlook.com>
2023-04-15 09:02:32 +0200

    Refactor : comment out age depending price

--- ajouter-des-proches/index.php
+++ ajouter-des-proches/index.php
@@ -16,10 +16,10 @@
     <link rel="stylesheet" href="../common.css">
     <link rel="stylesheet" href="./options-gala.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <script type="text/javascript">
+    <!-- <script type="text/javascript">
         const age = <?php echo $_SESSION['age'] ?>
     </script>
-    <script src="./price.js" defer></script>
+    <script src="./price.js" defer></script> -->
 </head>
 <body>
     <?php
@@ -30,13 +30,13 @@
     <main>
         <h2>Compléments facultatifs</h2>
         <form>
-            <p>Venez avec de votre petit•e frère/sœur, et faites-lui économiser jusqu'à 5€ !</p>
+            <!-- <p>Venez avec de votre petit•e frère/sœur, et faites-lui économiser jusqu'à 5€ !</p>
             <label><input type="checkbox" name="withSibling" value="true" oninput="updateTotal()">Oui, je souhaite l'accompagner !</label>
 
             <div id="final-price">
                 Votre prix total est : 
                 <span id="total"></span>
-            </div>
+            </div> -->
         </form>
     </main>
 </body>
