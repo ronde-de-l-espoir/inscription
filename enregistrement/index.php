5c465e5d5d57352dbbab2b36ef7e64b093475719
Elias Kirkwood <ek.mindstorms@outlook.com>
2023-05-06 20:17:52 +0200

    Feat : tickets download options

--- enregistrement/index.php
+++ enregistrement/index.php
@@ -77,9 +77,9 @@ foreach ($_SESSION['members']['table'] as $person) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LRDE -- Terminé</title>
     <link rel="stylesheet" href="../common.css">
-    <link rel="stylesheet" href="./modalites.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <script src="./modalites.js" defer></script>
+    <link rel="stylesheet" href="./enregistrement.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 
 <body>
@@ -90,7 +90,28 @@ foreach ($_SESSION['members']['table'] as $person) {
 
     <main>
         <h2>Terminé !</h2>
+        <p>Les tickets sont disponibles ci-dessous.</p>
+        <p>Vous pouvez les visionner en cliquant sur <span class="material-symbols-rounded" style="vertical-align: text-bottom;">visibility</span> ou les télécharger directement en cliquant sur <span class="material-symbols-rounded" style="vertical-align: text-bottom;">download</span></p>
+        <div id="tickets">
+            <div class="ticket">
+                <span><?= "gala-LRDE-" . $_SESSION['fname'] . "-" . $_SESSION['lname'] . "-" . $_SESSION['id'] . ".pdf" ?></span>
+                <div class="actions">
+                    <a href="../ticket/view/<?= $_SESSION['id'] ?>" target="_blank" style="color: inherit;"><span class="material-symbols-rounded">visibility</span></a>
+                    <a href="../ticket/dl/<?= $_SESSION['id'] ?>" target="_blank" style="color: inherit;"><span class="material-symbols-rounded">download</span></a>
+                </div>
+            </div>
+            <?php foreach ($_SESSION['members']['table'] as $member) : ?>
+            
+            <div class="ticket">
+                <span><?= "gala-LRDE-" . $member['fname'] . "-" . $member['lname'] . "-" . $member['id'] . ".pdf" ?></span>
+                <div class="actions">
+                    <a href="../ticket/view/<?= $member['id'] ?>" target="_blank" style="color: inherit;"><span class="material-symbols-rounded">visibility</span></a>
+                    <a href="../ticket/dl/<?= $member['id'] ?>" target="_blank" style="color: inherit;"><span class="material-symbols-rounded">download</span></a>
+                </div>
+            </div>
 
+            <?php endforeach ?>
+        </div>
     
     </main>
 </body>
