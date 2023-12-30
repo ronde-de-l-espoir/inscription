da35cb82cd6de6e818ee87b2596d74e1af31ec21
Elias Kirkwood <ek.mindstorms@outlook.com>
2023-12-30 18:35:15 +0100

    Comment ./modules

--- modules/checkIfIDExists.php
+++ modules/checkIfIDExists.php
@@ -1,17 +1,17 @@
 <?php
 
-require('../../db_config.php');
+require('../../db_config.php'); // connects to database
 
-$id = file_get_contents('php://input');
+$id = file_get_contents('php://input'); // strange way of getting a POST request's body...
 
 
 $sql = "SELECT COUNT(*) FROM `preinscriptions` WHERE `id`='$id';";
 
-$xResults = mysqli_fetch_all(mysqli_query($conn, $sql))[0][0];
+$xResults = mysqli_fetch_all(mysqli_query($conn, $sql))[0][0]; // number of results
 
 header('Content-Type: text/plain');
 if ($xResults != 0){
-    http_response_code(405);
+    http_response_code(405); // developer-defined response codes...I'm pretty sure that's not very good practice 🤔
 } else {
     http_response_code(205);
 }
--- modules/getDataFromSQL.php
+++ modules/getDataFromSQL.php
@@ -1,11 +1,18 @@
 <?php
-require('../../db_config.php');
+
+/**
+ * Returns all the information about a specified ID
+ * Including parent node and all the children nodes
+ * The script will always return $person, $children, $parent, even if they are empty to allow for looping in caller scripts
+ */
+
+require('../../db_config.php'); // connects to database
 $personSQL = "SELECT * FROM `preinscriptions` WHERE `id`='$requestID'";
 $personResult = mysqli_query($conn, $personSQL);
-$person = mysqli_fetch_assoc($personResult);
+$person = mysqli_fetch_assoc($personResult); // converts result into an associative array, similar to dicts in Python
 
 if ($person['nChildren'] > 0){
-    $childrenSQL = "SELECT * FROM `preinscriptions` WHERE `parentNode`='$requestID'";
+    $childrenSQL = "SELECT * FROM `preinscriptions` WHERE `parentNode`='$requestID'"; // queries the database for the list of children
     $childrenResult = mysqli_query($conn, $childrenSQL);
     $result = $conn->query($childrenSQL);
     $children = array();
@@ -13,16 +20,16 @@ if ($person['nChildren'] > 0){
         $children[] = $child;
     }
 } else {
-    $children = array();
+    $children = array(); // empty array
 }
 
-if ($person['parentNode'] != '' && $person['parentNode'] != '0'){
+if ($person['parentNode'] != '' && $person['parentNode'] != '0'){ // backwards compatibility where parentIDs where saved as empty strings
     $parentID = $person['parentNode'];
-    $parentSQL = "SELECT * FROM `preinscriptions` WHERE `id`='$parentID'";
+    $parentSQL = "SELECT * FROM `preinscriptions` WHERE `id`='$parentID'"; // gets the one and only parent node
     $parentResult = mysqli_query($conn, $parentSQL);
     $parent = mysqli_fetch_assoc($parentResult);
 } else {
-    $parent = array();
+    $parent = array(); // empty array
 }
 
 ?>
