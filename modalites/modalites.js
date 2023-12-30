c9ffb747cd9fd7cc753db36a3d07806502683fac
Elias Kirkwood <ek.mindstorms@outlook.com>
2023-12-30 11:10:39 +0100

    Comment ./modalites

--- modalites/modalites.js
+++ modalites/modalites.js
@@ -1,8 +1,8 @@
 function toggleSubmit(){
-    var sbmtBtn = document.querySelectorAll('button[value="continue"]')[0]
-    if (document.querySelectorAll('input[value="1"]')[0].checked){
-        sbmtBtn.disabled = false
+    var sbmtBtn = document.querySelectorAll('button[value="continue"]')[0] // gets the 'Next' button
+    if (document.querySelectorAll('input[value="1"]')[0].checked){ // checks if the confirmation checkbox is checked
+        sbmtBtn.disabled = false // enables the 'Next' button
     } else {
-        sbmtBtn.disabled = true
+        sbmtBtn.disabled = true // disables the 'Next' button
     }
 }
