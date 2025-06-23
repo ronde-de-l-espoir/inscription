300acb1b8a4ea7c56c281e1230a2116463b44326
Silloky (Elias K.) <ek.mindstorms@outlook.com>
2025-06-23 15:54:26 +0200

    ref: empty repository from v1

--- modalites/modalites.js
+++
@@ -1,8 +0,0 @@
-function toggleSubmit(){
-    var sbmtBtn = document.querySelectorAll('button[value="continue"]')[0] // gets the 'Next' button
-    if (document.querySelectorAll('input[value="1"]')[0].checked){ // checks if the confirmation checkbox is checked
-        sbmtBtn.disabled = false // enables the 'Next' button
-    } else {
-        sbmtBtn.disabled = true // disables the 'Next' button
-    }
-}
